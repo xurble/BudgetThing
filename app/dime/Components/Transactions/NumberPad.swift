2590d1dfa4ce9213e6d380f0dbfd15efc9a5234f
Gareth Simpson <g@xurble.org>
2026-03-07 15:57:11 +0000

    chore:numbers

--- app/dime/Components/Transactions/NumberPad.swift
+++ app/dime/Components/Transactions/NumberPad.swift
@@ -349,20 +349,22 @@ struct NumberPadTextView: View {
     var body: some View {
         HStack(alignment: .lastTextBaseline, spacing: 0) {
             Group {
-                Text(currencySymbol)
-                    .font(.system(.largeTitle, design: .rounded))
-                    .foregroundColor(Color.SubtitleText)
+                HStack(spacing: 0) {
+                    Text(currencySymbol)
+                        .font(.system(.largeTitle, design: .rounded))
+                        .foregroundColor(Color.SubtitleText)
 
-//                ForEach(displayNumbers, id: \.self) { number in
-//                    Text(number)
-//                        .font(.system(size: largerFontSize, weight: .regular, design: .rounded))
-//                        .foregroundColor(Color.PrimaryText)
-//                        .transition(AnyTransition.opacity.combined(with: .scale).combined(with: .move(edge: .trailing)))
-//                }
+//                    ForEach(displayNumbers, id: \.self) { number in
+//                        Text(number)
+//                            .font(.system(size: largerFontSize, weight: .regular, design: .rounded))
+//                            .foregroundColor(Color.PrimaryText)
+//                            .transition(AnyTransition.opacity.combined(with: .scale).combined(with: .move(edge: .trailing)))
+//                    }
 
-                + Text(amount)
-                    .font(.system(size: largerFontSize, weight: .regular, design: .rounded))
-                    .foregroundColor(Color.PrimaryText)
+                    Text(amount)
+                        .font(.system(size: largerFontSize, weight: .regular, design: .rounded))
+                        .foregroundColor(Color.PrimaryText)
+                }
 
             }
         }
